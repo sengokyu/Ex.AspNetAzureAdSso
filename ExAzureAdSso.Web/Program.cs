f7f3dcffeb2a7424fd982e6a560011eb36519f8a
sengokyu <sengokyu+gh@gmail.com>
2022-07-16 09:38:46 +0900

    Update main program.

--- ExAzureAdSso.Web/Program.cs
+++ ExAzureAdSso.Web/Program.cs
@@ -1,6 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Razorを使用するため
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// 静的ファイルを使用
+app.UseStaticFiles();
+// パスとRazorを紐付け
+app.MapRazorPages();
 
 app.Run();
